03d88b2283fe81d620cffa697314903a56c6a72e
Kaspar von Grünberg <kaspar@vongruenberg.com>
2019-09-10 17:01:39 +0200

    Update model_factories.py

--- products/tests/model_factories.py
+++ products/tests/model_factories.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 from ..models import Product, Property, Category
 
-#This is a comment that will do nothing
+#This is a comment that will do nothing but joy
 
 class ProductFactory(factory.DjangoModelFactory):
     workflowlevel2_uuid = factory.Faker('uuid4')
